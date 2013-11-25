4d75bbb05c8031cfb77614dcbb077e8e395f3c01
Adam Larat <adam.larat@gmail.com>
2013-11-26 00:25:23 +0100

    Rendering. Not properly working, but on the good way

--- public/javascripts/soldomi.js
+++ public/javascripts/soldomi.js
@@ -48,9 +48,12 @@ var SolDoMi = (function() {
   stave[0].setContext(ctx).draw();
   stave[1].setContext(ctx).draw();
 */
-    // Initialization. measuresById is a table of nStaves*nBlocks vexFlowStaves
-    tune.measuresById   = {};
+    // ************* Initialization ****************************** 
+    // measuresById is an object of object of nStaves*nBlocks vexFlowStaves. Its indices are the DB-ids
+    // measuresByCoord is an Array of Array of vexFlowStaves. Its indices are cartesian
+    tune.measuresById    = {};
     tune.measuresByCoord = new Array();
+/*
     // Count Staves and initialize measuresById array
     $.each(tune.systs,function(systNb,syst) {
       $.each(syst.staves,function(staffNb,staff) {
@@ -58,38 +61,42 @@ var SolDoMi = (function() {
 	tune.measuresByCoord.push(new Array());
       });
     });
-    // Count Blocks and start filling up VexFlow array
-    var staffCounter = 0;
+*/
+    // fill up VexFlow array. First indice is block.id 'cause we may usually want to render all staves but not necessarily all blocks
     $.each(tune.sects,function(sectNb,sect){
       $.each(sect.blocks,function(blockNb,block) {
+        tune.measuresById[block.id] = {};
+	blockNb = tune.measuresByCoord.push(new Array())-1; // push method of Array returns the length of the new Array. blockNb contains the correct indice
         $.each(tune.systs,function(systNb,syst) {
           $.each(syst.staves,function(staffNb,staff) {
-            tune.measuresById[staff.id][block.id] = new Vex.Flow.Stave(xCoor,yCoor,measureMinWidth);
-  	    tune.measuresByCoord[staffCounter].push(tune.measuresById[staff.id][block.id]);
+            tune.measuresById[block.id][staff.id] = new Vex.Flow.Stave(xCoor,yCoor,measureMinWidth).setContext(ctx).draw();
+            tune.measuresById[block.id][staff.id].segments = [];
+  	    tune.measuresByCoord[blockNb].push(tune.measuresById[block.id][staff.id]); // Reference to the VexFlow object in a cartesian reference.
             yCoor += lineHeight;
-	    staffCounter++;
           });
         });
-        xCoor += tune.measuresByCoord[0][blockNb].width;
+        xCoor += tune.measuresByCoord[blockNb][0].width;
         yCoor  = 0;
-	staffCounter = 0;
         $.getJSON("/block/"+block.id+"/symbols/json",renderBlock);
       });
     });
+    var nbBlocks = tune.measuresByCoord.length;
+    var nbStaves = tune.measuresByCoord[0].length;
     // Add First Blocks Ornaments (connectors, Clef, TimeSignature, etc)
-    tune.connectors = new Vex.Flow.StaveConnector(tune.measuresByCoord[0][0],tune.measuresByCoord[tune.measuresByCoord.length-1][0]);
+    tune.connectors = new Vex.Flow.StaveConnector(tune.measuresByCoord[0][0],tune.measuresByCoord[0][nbStaves-1]);
     tune.connectors.setType(Vex.Flow.StaveConnector.type.SINGLE);
     tune.connectors.setContext(ctx).draw();
     // Put double Bars at the end of each staff
-    $.each(tune.measuresByCoord,function(i,staff){
-             staff[staff.length-1].setEndBarType(Vex.Flow.Barline.type.END).setContext(ctx).draw();
+    $.each(tune.measuresByCoord[nbBlocks-1],function(i,staff){
+             staff.setEndBarType(Vex.Flow.Barline.type.END).setContext(ctx).draw();
     });
 
     // Render on Screen
-    $.each(tune.measuresByCoord,function(i,staff){
-	$.each(staff,function(j,block) {
-          block.setContext(ctx).draw();
-          Vex.Flow.Formatter.FormatAndDraw(ctx,block);
+    $.each(tune.measuresByCoord,function(blockNb,block){
+	$.each(block,function(staffNb,staff) {
+	  console.log(staff);
+     //     staff.setContext(ctx).draw();
+          Vex.Flow.Formatter.FormatAndDraw(ctx,staff,staff.segments,true);
         });
     });
 /*	  
@@ -119,23 +126,106 @@ var SolDoMi = (function() {
 */
   }
 
-  function renderBlock(symbols) {
-   console.log('ici');
-    var blockId,staffId;
+  var renderBlock = function(symbols) {
+  // console.log('ici');
+    var blockId=symbols[0].blockId,staffId;
+/*
+    $.each(tune.measuresById[blockId],function(measureNb,measure) {
+      measure.segments = [];
+    });
+*/
     $.each(symbols,function(i,symbol) {
       staffId = symbol.staffId; 
-      blockId = symbol.blockId; 
-      var measure = tune.measuresById[staffId][blockId];
+      var measure = tune.measuresById[blockId][staffId];
       renderSymbol(symbol,measure);
     });
-    $.each(tune.measuresById,function(staffNb,staff) {
-	staff[blockId].setContext(ctx).draw();
+
+    $.each(tune.measuresById[blockId],function(staffNb,staff) {
+	staff.setContext(ctx).draw();
     });
+
     return;
   }
 
-  function renderSymbol(symbol,measure) {
+  var renderSymbol =function(symbol,measure) {
+    var type = symbol.type;
+    console.log(type);
+    switch(type) {
+      case "TREBLE_CLEF":    
+	measure.addClef("treble");
+	break;
+      case "BASS_CLEF":      
+	measure.addClef("bass");
+	break;
+      case "KEY_SIGNATURE":  
+//	measure.addKeySignature(formatKeySignature(symbol));
+	break;
+      case "STANDARD_TIME_SIGNATURE": 
+	measure.addTimeSignature(formatTimeSignature(symbol));
+	break;
+      case "WHOLE":
+      case "HALF":
+      case "QUARTER": // Vaudrait mieux que ce soit "Note" et que le type soit géré que par duration
+      case "EIGTH":
+      case "SIXTEENTH":
+      case "SEGMENT":
+	measure.segments.push(createNewSegment(symbol));
+	break;
+      default:
+        console.log("Error, wrong type: ",type);
+    }
     return;  
+  }
+
+  var formatKeySignature = function (symbol) {
+    return "";
+  }
+
+  var formatTimeSignature = function (symbol) {    
+    var count,value;
+    count = symbol.timeSignature.beatCount;
+    switch(symbol.timeSignature.beatValue) {
+    case "QUARTER":
+      value = "4";
+      break;
+    default: 
+      consol.log("Please inform what this beatValue is:",symbol.timeSignature.beatValue);
+      break;
+    }
+    return count+"/"+value;
+  }
+
+  var createNewSegment = function (segment) {
+    return new Vex.Flow.StaveNote(
+	{ keys :    [formatKeys(segment)],
+	  duration: formatDuration(segment.note.duration)
+	//  clef:     formatClef(segment),
+          //stem_direction: -1
+      	});
+  }
+
+  var formatKeys = function (segment) {
+    return segment.note.pitch.name.toLowerCase()+formatAccidental(segment.note.accidental)
+             +'/'+segment.note.pitch.octave;
+  }
+
+  var formatAccidental = function(accidental) {
+    switch(accidental) {
+    case "AUTO": return "";
+    default: 
+      console.log("Accidental problem: ", accidental);
+      break;
+    }
+  }
+
+  var formatDuration = function (duration) {
+    var n = duration.n;
+    var d = duration.d;
+
+    return duration.n.toString();
+  }
+
+  var formatClef = function (segment) {
   }
 
   // Public
